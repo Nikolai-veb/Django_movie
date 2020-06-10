9229ae35bdae8804a80f89a4170a01f11df67a6d
nkolai <nik.vagin1995@mail.ru>
2020-06-11 00:29:07 +0300

    new file < static plus templates

--- django_movie/movie/views.py
+++ django_movie/movie/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
 from .models import Movie
 
 
-class MoviesView(Views):
+class MoviesView(View):
 
     def get(self, request):
         movie = Movie.objects.all()
