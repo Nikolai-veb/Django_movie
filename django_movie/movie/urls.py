e770e94945a6217820ce608289b4bbc6a062ee47
Nikolai <nik.vagin1995@mail.ru>
2020-06-13 13:24:23 +0300

    modified templates, create forms.py and views Reviews. Add movieshots

--- django_movie/movie/urls.py
+++ django_movie/movie/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-
+from django.conf.urls import url
 
 urlpatterns = [
 
-    path("", views.MoviesView.as_view()),
-    path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
+    url(r"^$", views.MoviesView.as_view()),
+    url("<slug:slug>", views.MovieDetailView.as_view(), name="movie_detail"),
+    url("review/(?P<pk>\d+)$", views.AddReview.as_view(), name="add_review"),
 ]
