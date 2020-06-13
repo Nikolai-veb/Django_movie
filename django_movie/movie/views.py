1a1b4903728e86c752c8dabe1348c0c35e74a78b
Nikolai <nik.vagin1995@mail.ru>
2020-06-13 04:29:35 +0300

    Add get_absolute_url, ListView and DetailView  model = Movie

--- django_movie/movie/views.py
+++ django_movie/movie/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.base import View
-
+from django.views.generic import ListView, DetailView
 from .models import Movie
 
 
-class MoviesView(View):
+class MoviesView(ListView):
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    
 
-    def get(self, request):
-        movie = Movie.objects.all()
-        return render(request, 'movies/movie.html', {'movie_list':movie})
 
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_field = 'url'
