e21e7c51873b282b6d2129637e6409908c349af3
Nikolai <nik.vagin1995@mail.ru>
2020-06-14 13:54:22 +0300

    create views GenreYear filter

--- django_movie/movie/urls.py
+++ django_movie/movie/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import url
 urlpatterns = [
 
     url(r"^$", views.MoviesView.as_view()),
+    path("filter/", views.FilterMoviesView.as_view(), name='filter'),
     url("<slug:slug>", views.MovieDetailView.as_view(), name="movie_detail"),
     url("review/(?P<pk>\d+)$", views.AddReview.as_view(), name="add_review"),
     path("actor/<str:slug>/", views.ActorView.as_view(), name="actor_detail")
--- django_movie/movie/views.py
+++ django_movie/movie/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
-from .models import Movie, Actor
+from .models import Movie, Actor, Category, Ganre
 from .forms import ReviewForm
 
-class MoviesView(ListView):
+
+class GanreYear:
+    """Жанры и года выхода Фильмов"""
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values("year")
+
+
+class MoviesView(GanreYear, ListView):
     model = Movie
     queryset = Movie.objects.filter(draft=False)
     
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GanreYear, DetailView):
     model = Movie
     slug_field = 'url'
 
@@ -30,8 +40,17 @@ class AddReview(View):
 
 
 
-class ActorView(DetailView):
+class ActorView(GanreYear, DetailView):
     """Вывод детальнтй информацыи о актере"""
     model = Actor
     template_name = 'movie/actor.html'
     slug_fields = "name"
+
+
+class FilterMoviesView(GanreYear, ListView):
+    """Фильтр фильмов"""
+    def get_queryset(self):
+        queryset = Movie.objects.filter(Q(year__in=self.request.GET.getlist("year"))|Q(genres__in=self.request.GET.getlist("genre")))
+        return queryset
+
+
